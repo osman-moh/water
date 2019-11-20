fc0e62ea45802e6910f520f2b56a2664c34d6f99
Mknan-fms <mustafa.ibrah93@gmail.com>
2019-11-20 20:04:14 +0200

    Enhanced sidebar view

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -5,87 +5,78 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
         <ul class="sidebar-menu">
-          <li  id="tour_step4">
-            <a href="{{route('home')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>القائمة الرئيسة</span>
-            </a>
-          </li>
-            <li  id="tour_step4">
-                <a href="{{route('reports.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>البلاغات</span>
-                </a>
+            <li class="treeview">
+                <a href="{{route('home')}}"><i class="fa fa-home"></i><span>القائمة الرئيسة</span></a>
             </li>
+
+            <li class="treeview">
+                <a href="{{route('reports.index')}}"><i class="fa fa-cubes"></i><span>البلاغات</span></a>
+            </li>
+
             @if (auth()->user()->user_type_id == 1)
-            <li  id="tour_step4">
-              <a href="{{route('locations.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                مواقع البلاغلات
-              </span>
-              </a>
+
+            <li class="treeview">
+                <a href="#"><i class="fa fa-cogs"></i><span>إعدادات البلاغات</span></a>
+                <ul class="treeview-menu">
+                  <li class="treeview">
+                      <a href="{{route('locations.index')}}"><i class="fa fa-circle-o"></i><span>مواقع البلاغلات</span></a>
+                  </li>
+
+                  <li class="treeview">
+                      <a href="{{ route('category.index') }}"><i class="fa fa-circle-o"></i><span>تصنيف البلاغات</span></a>
+                  </li>
+
+                  <li class="treeview">
+                      <a href="{{ route('reportType.index') }}"><i class="fa fa-circle-o"></i><span>انواع البلاغ</span></a>
+                  </li>
+
+                  <li class="treeview">
+                      <a href="{{ route('report_sub_types.index') }}"><i class="fa fa-circle-o"></i><span>البنود الفرعية للبلاغات</span></a>
+                  </li>
+                  <li class="treeview">
+                      <a href="{{ route('report_sub_details.index') }}"><i class="fa fa-circle-o"></i><span>تفاصيل البنود الفرعية</span></a>
+                  </li>
+                </ul>
             </li>
-            <li  id="tour_step4">
-              <a href="{{route('category.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                تصنيف البلاغات
-              </span>
-              </a>
-            </li>
-            <li  id="tour_step4">
-              <a href="{{route('cities.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                  المناطق الكبري
-              </span>
-              </a>
-            </li>
-            <li  id="tour_step4">
-              <a href="{{route('localities.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                    المحليات
-              </span>
-              </a>
-            </li>
-            <li  id="tour_step4">
-              <a href="{{route('offices.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                    المكاتب الفرعية
-              </span>
-              </a>
-            </li>
-            <li  id="tour_step4">
-              <a href="{{route('towns.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                    المدن
-              </span>
-              </a>
-            </li>
-            <li  id="tour_step4">
-              <a href="{{route('squares.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                    المربعات
-              </span>
-              </a>
-            </li>
-            <li  id="tour_step4">
-                <a href="{{route('usersType.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                  تصنيف المستخدمين
-                </span>
-                </a>
-              </li>
-            <li  id="tour_step4">
-              <a href="{{route('user.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                    المستخدمين
-              </span>
-              </a>
-            </li>
-            <li id="tour_step4">
-                <a href="{{route('reportType.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                      انواع البلاغ
-                </span>
-                </a>
-            </li>
-            <li id="tour_step4">
-                <a href="{{route('report_sub_types.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                    البنود الفرعية للبلاغات
-                </span>
-                </a>
-            </li>
-            <li id="tour_step4">
-              <a href="{{route('report_sub_details.index')}}" id="tour_step4_menu"><i class="fa fa-fw fa-cube"></i> <span>
-                  تفاصيل البنود الفرعية
-              </span>
-              </a>
-          </li>
+
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-map"></i><span>إعدادات المناطق</span></a>
+                    <ul class="treeview-menu">
+                      <li class="treeview">
+                          <a href="{{ route('cities.index') }}"><i class="fa fa-circle-o"></i><span>المناطق الكبري</span></a>
+                      </li>
+                      <li class="treeview">
+                          <a href="{{ route('localities.index') }}"><i class="fa fa-circle-o"></i><span>المحليات</span></a>
+                      </li>
+                      <li class="treeview">
+                          <a href="{{ route('offices.index') }}"><i class="fa fa-circle-o"></i><span>المكاتب الفرعية</span></a>
+                      </li>
+                      <li class="treeview">
+                          <a href="{{ route('towns.index') }}"><i class="fa fa-circle-o"></i><span>المدن</span></a>
+                      </li>
+                      <li class="treeview">
+                          <a href="{{ route('squares.index') }}"><i class="fa fa-circle-o"></i><span>المربعات</span></a>
+                      </li>
+                    </ul>
+                </li>
+
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-users"></i><span>إعدادات المستخدمين</span></a>
+                    <ul class="treeview-menu">
+                      <li class="treeview"><a href="{{ route('user.index') }}"><i class="fa fa-circle-o"></i><span>المستخدمين</span></a></li>
+                      <li class="treeview"><a href="{{ route('usersType.index') }}"><i class="fa fa-circle-o"></i><span>تصنيف المستخدمين</span></a></li>
+                    </ul>
+                </li>
+
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-dashboard"></i><span>التقارير</span></a>
+                    <ul class="treeview-menu">
+                      <li class="treeview"><a href="{{ route('reports.index') }}"><i class="fa fa-bar-chart"></i><span>التقارير العامة</span></a></li>
+                      <li class="treeview"><a href="{{ route('reports.index') }}"><i class="fa fa-file-o"></i><span>إنشاء تقرير</span></a></li>
+                      <li class="treeview"><a href="{{ route('reports.index') }}"><i class="fa fa-circle-o"></i><span>تقارير GIS</span></a></li>
+                    </ul>
+                </li>
+            
             @endif
           </ul>
     </section>
